2e9b6bba332eab224e08830d31cdfe9d683ad417
Champ <ichamp_in@hotmail.com>
2025-03-02 20:33:31 +0700

    'initiate_ingestion_data'

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb =error_detail.exc_info()
     file_name =exc_tb.tb_frame.f_code.co_filename
@@ -15,9 +15,3 @@ class Custom_exception(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        1/0
-    except Exception as e:
-        logging.info("cant divide by zero")
-        raise Custom_exception(e,sys)
